94c8d1e47cf1c420b424f94db013e5e0083d438a
LEEJIWOO <moebius.jw@gmail.com>
2013-12-08 21:05:18 +0900

    .

---
+++ phonegapTest0223/assets/www/js/relation-canvas.js
@@ -0,0 +1,178 @@
+//console.log("relation-canvasjs...");
+//
+//function first(ctx, yn){
+//	if(yn){
+//
+//		  ctx.beginPath();
+//		  ctx.lineWidth="5";
+//		  ctx.strokeStyle="blue"; //#33B5E5
+//		  ctx.moveTo(55, 70);
+//		  ctx.lineTo(240, 70);
+//		  ctx.stroke();
+//
+//	} else {
+//
+//	}
+//};
+//
+//function second(ctx, yn) {
+//	if (yn) {
+//		 ctx.beginPath();
+//		  ctx.lineWidth="5";
+//		  ctx.strokeStyle="blue";
+//		  ctx.moveTo(250, 55);
+//		  ctx.lineTo(250, 250);
+//		  ctx.stroke();
+//
+//	} else {
+//
+//	}
+//};
+//
+//function third(ctx, yn) {
+//	if (yn) {
+//		 ctx.beginPath();
+//		  ctx.lineWidth="5";
+//		  ctx.strokeStyle="blue";
+//		  ctx.moveTo(235, 235);
+//		  ctx.lineTo(70, 235);
+//		  ctx.stroke();
+//	} else {
+//
+//	}
+//};
+//
+//
+//function fourth(ctx, yn) {
+//	if (yn) {
+//
+//		 ctx.beginPath();
+//		  ctx.lineWidth="5";
+//		  ctx.strokeStyle="blue";
+//		  ctx.moveTo(70, 240);
+//		  ctx.lineTo(70, 55);
+//		  ctx.stroke();
+//
+//
+//	} else {
+//
+//	}
+//};
+//
+//function horizontalLine(ctx, yn) {
+//	if (yn) {
+//		 ctx.beginPath();
+//		  ctx.lineWidth="5";
+//		  ctx.strokeStyle="blue";
+//		  ctx.moveTo(250, 240);
+//		  ctx.lineTo(70, 70);
+//		  ctx.stroke();
+//	} else {
+//
+//	}
+//};
+//
+//function verticalLine(ctx, yn) {
+//	if (yn) {
+//		  ctx.beginPath();
+//	  ctx.lineWidth="5";
+//	  ctx.strokeStyle="blue";
+//	  ctx.moveTo(250, 70);
+//	  ctx.lineTo(70, 240);
+//	  ctx.stroke();
+//	} else {
+//
+//	}
+//};
+
+
+
+/**********************************************************/
+
+
+function first(ctx, yn){
+if(yn){
+
+	  ctx.beginPath();
+	  ctx.lineWidth="5";
+	  ctx.strokeStyle="crimson";
+	  ctx.moveTo(55, 55);
+	  ctx.lineTo(270, 55);
+	  ctx.stroke();
+
+} else {
+
+}
+};
+
+function second(ctx, yn){
+if (yn) {
+	 ctx.beginPath();
+	  ctx.lineWidth="5";
+	  ctx.strokeStyle="crimson";
+	  ctx.moveTo(270, 55);
+	  ctx.lineTo(270, 270);
+	  ctx.stroke();
+
+} else {
+
+}
+};
+
+function third(ctx, yn){
+if (yn) {
+
+	 ctx.beginPath();
+	  ctx.lineWidth="5";
+	  ctx.strokeStyle="crimson";
+	  ctx.moveTo(270, 270);
+	  ctx.lineTo(55, 270);
+	  ctx.stroke();
+
+
+} else {
+
+}
+};
+
+function fourth(ctx, yn){
+if (yn) {
+
+	 ctx.beginPath();
+	  ctx.lineWidth="5";
+	  ctx.strokeStyle="crimson";
+	  ctx.moveTo(55, 270);
+	  ctx.lineTo(55, 55);
+	  ctx.stroke();
+
+
+} else {
+
+}
+};
+
+function horizontalLine(ctx, yn){
+if (yn) {
+	 ctx.beginPath();
+	  ctx.lineWidth="5";
+	  ctx.strokeStyle="crimson";
+	  ctx.moveTo(270, 270);
+	  ctx.lineTo(55, 55);
+	  ctx.stroke();
+} else {
+
+}
+};
+
+function verticalLine(ctx, yn){
+if (yn) {
+	ctx.beginPath();
+	  ctx.lineWidth="5";
+	  ctx.strokeStyle="crimson";
+	  ctx.moveTo(270, 55);
+	  ctx.lineTo(55, 270);
+	  ctx.stroke();
+} else {
+
+}
+};
